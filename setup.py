17cba45e86bf234591fbdef17904e073bf3398da
Josh <josh@devops.center>
2015-05-04 07:29:33 -0700

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-code-deploy',
+  packages = ['django-code-deploy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Deploys Django code',
+  author = 'Josh devops.center',
+  author_email = 'josh@devops.center',
+  url = 'https://github.com/devopscenter/django-code-deploy', # use the URL to the github repo
+  download_url = 'https://github.com/devopscenter/django-code-deploy/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
