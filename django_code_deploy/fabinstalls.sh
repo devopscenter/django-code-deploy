9dd2ea43fca6a656023fd49ae6665bdbaca7576c
Josh <josh@devops.center>
2015-06-09 22:22:44 -0700

    cleanup

--- django_code_deploy/fabinstalls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-fab -R prod-web -P docker_web_install
-fab -R prod-masterworker -P docker_worker_master_install
-fab -R prod-standbyworker -P docker_worker_standby_install
-fab -R prod-masterdb -P docker_masterdb_install
-fab -R prod-standbydb -P docker_standbydb_install
