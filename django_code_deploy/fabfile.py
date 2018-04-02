a5d03f56117df4a44e1bf7fd9ed591d05a6f8dbb
Gregg Jensen <gjensen@devops.center>
2018-04-02 14:12:46 -0500

    more debugging...it still doesn't work.

--- django_code_deploy/fabfile.py
+++ django_code_deploy/fabfile.py
@@ -307,19 +307,19 @@ def collect_static():
 def setup_server_symlinks(aPath):
     pathToInstallFrom = '/media/data/deploy/current/' + aPath
     pathToInstallTo = '/media/data/deploy/current/' + aPath + "/dist"
-    sudo('if [[ -d %s/node_modules ]]; then ln -s %s/node_modules %s/node_modules; fi' %
+    sudo('if [[ -d %s/node_modules ]]; then ln -s %s/node_modules %s/node_modules ; else; echo "1"; fi' %
          (pathToInstallFrom, pathToInstallFrom, pathToInstallTo))
 
-    sudo('if [[ -f %s/config/settings.js ]]; then ln -s %s/config/settings.js %s/settings.js; fi' %
+    sudo('if [[ -f %s/config/settings.js ]]; then ln -s %s/config/settings.js %s/settings.js ; else ; echo "2"; fi' %
          (pathToInstallFrom, pathToInstallFrom, pathToInstallTo))
 
-    sudo('if [[ -d %s/public/metaswitch ]]; then ln -s %s/public/metaswitch %s/metaswitch; fi' %
+    sudo('if [[ -d %s/public/metaswitch ]]; then ln -s %s/public/metaswitch %s/metaswitch ; else ; echo "3"; fi' %
          (pathToInstallFrom, pathToInstallFrom, pathToInstallTo))
 
-    sudo('if [[ -d %s/public/nec ]]; then ln -s %s/public/nec %s/nec; fi' %
+    sudo('if [[ -d %s/public/nec ]]; then ln -s %s/public/nec %s/nec ; else; echo "4"; fi' %
          (pathToInstallFrom, pathToInstallFrom, pathToInstallTo))
 
-    sudo('if [[ -f %s/public/login.html ]]; then ln -s %s/public/login.html %s/login.html; fi' %
+    sudo('if [[ -f %s/public/login.html ]]; then ln -s %s/public/login.html %s/login.html ; else; echo "5";  fi' %
          (pathToInstallFrom, pathToInstallFrom, pathToInstallTo))
 
 
