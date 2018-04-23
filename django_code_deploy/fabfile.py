24b917ab999a2cf8b9b5c047d68c1772c464ed67
Gregg Jensen <gjensen@devops.center>
2018-04-23 15:48:33 -0500

    add copying another set of configs to the distribution directory.

--- django_code_deploy/fabfile.py
+++ django_code_deploy/fabfile.py
@@ -274,6 +274,8 @@ def build(branch, installPath, node=False, angularBuild=False):
                 'if [[ -d "config" ]]; then echo "<collecting config>" ; rsync -ra --stats config/ dist/config; fi;')
             local(
                 'if [[ -d "src/public" ]]; then echo "<collecting public>" ; rsync -ra --stats src/public/ dist/public; fi;')
+            local(
+                'if [[ -d "src/dt-cfgs" ]]; then echo "<collecting dt-cfgs>" ; rsync -ra --stats src/dt-cfgs/ dist/dt-cfgs; fi;')
 
     # make sure the new files are part of the local git repo
     local('find . -path ./\.git -prune -o -name \.gitignore -type f -exec rm -f {} \;')
