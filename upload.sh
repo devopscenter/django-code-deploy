afba635de2b72d3700bd6c0f5bf87d50de478abe
Bob Lozano <boblozano@gmail.com>
2018-05-01 18:38:54 -0500

    change upload to conform to updated pypi procedures

--- upload.sh
+++ upload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ev
 
-#http://peterdowns.com/posts/first-time-with-pypi.html
+python setup.py sdist
 
-python setup.py register -r pypi
-
-python setup.py sdist upload -r pypi
+twine upload dist/*
+ 
